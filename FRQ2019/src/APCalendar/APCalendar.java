ab48d8e60401a734f9f284252be9a3b9e88ab5a6
charles d <charliedegeorge@gmail.com>
2024-11-07 11:07:10 -0500

    changed frq 2019 to actually work this time

--- FRQ2019/src/APCalendar/APCalendar.java
+++ FRQ2019/src/APCalendar/APCalendar.java
@@ -3,56 +3,193 @@ package APCalendar;
 import java.util.*;
 
 public class APCalendar {
-	
-	//constructor
+
+	// constructor
 	public APCalendar() {
-		
+
 	}
-	
-	// YES if it's divisible by 4; NO if divisible by 4 && 100; YES if divisible by 400;
-	
+
+	// YES if it's divisible by 4; NO if divisible by 4 && 100; YES if divisible by
+	// 400;
+
 	public boolean isLeapYear(int year) {
-		if ((year % 4 == 0 && year % 100 != 0)||(year % 400 == 0)){
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public int numberOfLeapYears(int year, int year2) {
 		int numOfLeaps = 0;
 		int numYears = year2 - year;
 		int[] years = new int[numYears];
 		for (int i = 0; i < years.length; i++) {
-			years[i] = year+i;
+			years[i] = year + i;
 		}
-		
+
 		for (int i = 0; i < years.length; i++) {
 			if (isLeapYear(years[i])) {
 				numOfLeaps++;
 			}
 		}
-		
-		
+
 		return numOfLeaps;
 	}
-	
+
 	public int firstDayOfYear(int year) {
 		int numShifts = (year - 1933);
-		System.out.println(numShifts);
-		System.out.println();
+		int offset = numberOfLeapYears(year, year + numShifts);
+		numShifts = numShifts + offset;
 		int loops = 0;
 		if (numShifts >= 0) {
-			loops = (numShifts % 6);
-			System.out.println(loops);
-			System.out.println();
+			loops = Math.abs((numShifts % 7));
+		} else if (numShifts < 0) {
+			loops = (Math.abs((numShifts % 7)));
 		}
-		else if (numShifts < 0) {
-			loops = (Math.abs((numShifts % 6)))+1;
-			System.out.println(loops);
-			System.out.println();
-		}
-		// TODO resolve issue going from 2017 to 2016, loops never hits 0
 		return loops;
 	}
-	
+
+	public int dayOfYear(int day, int month, int year) {
+		if (month == 2) {
+			day += 31;
+		}
+		if (month == 3) {
+			day += 31;
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			
+		}
+		if (month == 4) {
+			// 30
+			day += 31;
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+		}
+		if (month == 5){
+			day += 31;
+			// 31
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+			day += 31;
+		}
+		if (month == 6) {
+			day += 31;
+			// 30
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 30;
+		}
+		if (month == 7) {
+			day += 31;
+			// 31
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 30;
+			day += 31;
+		}
+		if (month == 8) {
+			day += 31;
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 31;
+		}
+		if (month == 9) {
+			day += 31;
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 31;
+			day += 30;
+		}
+		if (month == 10) {
+			day += 31;
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 31;
+			day += 30;
+		}
+		if (month == 11) {
+			day += 31;
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 31;
+			day += 30;
+			day += 31;
+		}
+		if (month == 12) {
+			day += 31;
+			if (isLeapYear(year)) {
+				day += 29;
+			} else
+				day += 28;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 30;
+			day += 31;
+			day += 31;
+			day += 30;
+			day += 30;
+			day += 31;
+		}
+		return day;
+
+	}
+
+	public int dayOfWeek(int day, int month, int year) {
+		int start = firstDayOfYear(year);
+		// System.out.println(start);
+		int thing = dayOfYear(day, month, year);
+		System.out.println(thing);
+		int weekday = (thing % 7) + start-1;
+
+		return weekday;
+	}
 }
--- FRQ2019/src/APCalendar/APCalendarTester.java
+++ FRQ2019/src/APCalendar/APCalendarTester.java
@@ -7,7 +7,8 @@ public class APCalendarTester {
 		
 		//System.out.println(calendarey.isLeapYear(2020));
 		//System.out.println(calendarey.numberOfLeapYears(1997, 2025));
-		System.out.println(calendarey.firstDayOfYear(2018));
+		//System.out.println(calendarey.firstDayOfYear(2023));
+		System.out.println(calendarey.dayOfWeek(1, 11, 2024));
 	}
 
 }
